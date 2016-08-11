fcec15d756e9ef06ccc0941354b163c2258f9eca
Bibash Kafle <BbashKafle>
2016-08-11 13:00:03 +0545

    Project Added

---
+++ Diff Checker/Default.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Diff_Checker.Default" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+           Text 1: <asp:TextBox runat="server" ID="text1" TextMode="MultiLine" Width="300" Height="30"></asp:TextBox><br />
+           Text 2: <asp:TextBox runat="server" ID="text2" TextMode="MultiLine" Width="300" Height="30"></asp:TextBox><br />
+           <asp:Button runat="server" ID="btnCheck" Text="Check" onclick="btnCheck_Click" /><br />
+        <div id="result" runat="server"></div>
+    </div>
+    </form>
+</body>
+</html>
---
+++ Diff Checker/Default.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Diff_Checker
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnCheck_Click(object sender, EventArgs e)
+        {
+            var dmp = new diff_match_patch();            
+            var list = dmp.diff_main(text1.Text, text2.Text);
+            var line1 = dmp.GetDifferent(list);
+            var line2 = dmp.GetDifferent(list,false);           
+            dmp.diff_cleanupSemantic(list);
+
+            var str =  "<br>OLD = "+dmp.diff_prettyHtml(line1);
+            str += "<br>New = " + dmp.diff_prettyHtml(line2);
+            str += "<br><br><br><b>Single Line</b><br>" + dmp.diff_prettyHtml(list);
+            result.InnerHtml = str;
+        }
+    }
+}
